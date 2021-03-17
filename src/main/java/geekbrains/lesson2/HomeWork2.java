2821cf4eb581d682a93c67a22bea3050d6dae10a
avk61rus <avk61rus@gmail.com>
2021-03-17 18:06:56 +0300

    Homework 2

--- src/main/java/geekbrains/lesson2/HomeWork2.java
+++ src/main/java/geekbrains/lesson2/HomeWork2.java
@@ -1,7 +1,29 @@
 package geekbrains.lesson2;
 
-public class HomeWork2 {
+public class HomeWork2<arr1> {
     public static void main(String[] args) {
-        System.out.println("начало");
+//        System.out.println("начало");
+        replacingValues();                      // for task 1
+
+
+
     }
+
+
+
+    // task #1
+    /* -----------------------------------------------------*/
+    private static void replacingValues() {
+        int[] arr1 = {1, 1, 0, 1, 0, 0, 1, 0, 1, 1};
+        int i = arr1.length;
+
+
+
+
+        System.out.println(i);
+    }
+
+
+
+
 }
